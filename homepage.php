d9d6284f5dbce151e941df55ba270c1ab4aed788
althirdaranilla <althirdaranilla@gmail.com>
2026-01-12 22:54:26 +0800

    Update homepage.php
    Updated the homepage to reflect new branding and content.

--- homepage.php
+++ homepage.php
@@ -13,7 +13,7 @@ $password = 'Q5b&kOh+2';
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Barangay Management System</title>
+  <title>Barangay 468</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link rel="icon" href="favicon.ico" type="image/x-icon">
@@ -347,4 +347,5 @@ $password = 'Q5b&kOh+2';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
 
